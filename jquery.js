cac7d20d68d9844a8df246e59be6d53cc9077cae
Luis <luiscarlos10gto@hotmail.com>
2022-06-27 02:18:03 -0300

    Melhoria no layout e jQuery

--- jquery.js
+++ jquery.js
@@ -1,33 +1,35 @@
-$(function(){
-    $('#formacao').hide();
-    $('#projetos').hide();
-    $('#contato').hide();
-
-    $('#btnquemsoueu').click(function(){
-        $('#quemsoueu').fadeIn(2000);
+$(document).ready(function(){
+    $(function(){
         $('#formacao').hide();
         $('#projetos').hide();
         $('#contato').hide();
-    })
 
-    $('#btnformacao').click(function(){
-        $('#quemsoueu').hide();
-        $('#formacao').fadeIn(2000);
-        $('#projetos').hide();
-        $('#contato').hide();
-    })
+        $("#btnquemsoueu").click(function(){
+            $('#quemsoueu').fadeIn(2000);
+            $('#formacao').hide();
+            $('#projetos').hide();
+            $('#contato').hide();
+        })
 
-    $('#btnprojetos').click(function(){
-        $('#quemsoueu').hide();
-        $('#formacao').hide();
-        $('#projetos').fadeIn(2000);
-        $('#contato').hide();
-    })
+        $('#btnformacao').click(function(){
+            $('#quemsoueu').hide();
+            $('#formacao').fadeIn(2000);
+            $('#projetos').hide();
+            $('#contato').hide();
+        })
 
-    $('#btncontato').click(function(){
-        $('#quemsoueu').hide();
-        $('#formacao').hide();
-        $('#projetos').hide();
-        $('#contato').fadeIn(2000);
+        $('#btnprojetos').click(function(){
+            $('#quemsoueu').hide();
+            $('#formacao').hide();
+            $('#projetos').fadeIn(2000);
+            $('#contato').hide();
+        })
+
+        $('#btncontato').click(function(){
+            $('#quemsoueu').hide();
+            $('#formacao').hide();
+            $('#projetos').hide();
+            $('#contato').fadeIn(2000);
+        })
     })
 });
