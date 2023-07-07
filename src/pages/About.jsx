da5b26dd5c11bc2f53e2535b0f3cd4764f1b9d3d
lolisdev <lolisdev@gmail.com>
2023-07-07 15:00:13 -0300

    Melhoria na responsividade.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,39 +4,41 @@ import Perfil from '../assets/perfil3.jpg'
 export default function Curso(props) {
     return (
         <div className="container">
-            <div className="row quemsoueu">
-                <div className="col align-self-center">
-                    <div className="row pt-md-5">
-                        <div className="col-12 pt-md-5">
-                            <p className=" h1 text-center pt-5"><span className="text-danger">WEB</span>
-                            <span className="text-white">DEVELOPER</span></p>
-                        </div>
-                        <div className="col-12 showmobile">
-                            <p className="text-center pt-3">
-                                <img src={Perfil} alt='Imagem de Perfil' className="w-75 rounded-circle p-2 imgcinza" />
-                            </p>
-                        </div>
-                        <div className="col-12 pt-md-5 pt-4">
-                            <p className="text-white">Olá, sou o Luis. Seja bem vindo(a)! Sou graduado em <strong>Ciência da Computação</strong> com especialização na área de Desenvolvimento Web <strong>Full Stack</strong> e <strong>Cloud Computing.</strong></p>
+            <div className="row">
+                <div className="col quemsoueu">
+                    <div className="col align-self-center">
+                        <div className="row">
+                            <div className="col-12 pt-md-3">
+                                <p className=" h1 text-center pt-5"><span className="text-danger">WEB</span>
+                                    <span className="text-white">DEVELOPER</span></p>
+                            </div>
+                            <div className="col-12 showmobile">
+                                <p className="text-center pt-3">
+                                    <img src={Perfil} alt='Imagem de Perfil' className="w-75 rounded-circle p-2 imgcinza" />
+                                </p>
+                            </div>
+                            <div className="col-12 pt-md-5 pt-4">
+                                <p className="text-white">Olá, sou o Luis. Seja bem vindo(a)! Sou graduado em <strong>Ciência da Computação</strong> com especialização na área de Desenvolvimento Web <strong>Full Stack</strong> e <strong>Cloud Computing.</strong></p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="col align-self-center pt-5 showweb mt-5 d-flex justify-content-center">
-                    <img src={Perfil} alt="Imagem de Perfil" className="w-75 rounded-circle imgcinza m-5" />
-                </div>
+                    <div className="col align-self-center pt-5 showweb justify-content-center">
+                        <img src={Perfil} alt="Imagem de Perfil" className="w-75 rounded-circle imgcinza m-5" />
+                    </div>
 
-                <div className="row text-white align-self-end justify-content-center d-flex p-0 m-0">
-                    <div className="col-6 text-end">
+                    <div className="row text-white align-self-end justify-content-center d-flex p-0 m-0">
+                        <div className="col-6 text-end">
 
-                        {/* <a href="http://github.com/lolisdev" target="_blank">
+                            {/* <a href="http://github.com/lolisdev" target="_blank">
                             <i className="fab fa-github"></i>
                         </a> */}
-                    </div>
-                    <div className="col-6 text-start">
-                        {/* <a href="https://www.linkedin.com/in/luis-carlos-pereira" target="_blank">
+                        </div>
+                        <div className="col-6 text-start">
+                            {/* <a href="https://www.linkedin.com/in/luis-carlos-pereira" target="_blank">
                             <i className="fab fa-linkedin"></i>
                         </a> */}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -21,23 +21,22 @@ export default function Header() {
     <header>
       <nav className="navbar navbar-expand-lg fixed-top bg-primary-color" id="navbar">
         <div className="container">
-          <a href="#gh" className="navbar-brand d-flex align-items-center">
+          <a href="https://lolisdev.github.io" className="navbar-brand d-flex align-items-center">
             <i className="fa-solid fa-code showweb"></i>
             <i className="fa-solid fa-code showmobile"></i>
             <span>Luis Carlos</span>
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-items"
+          <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-items"
             aria-controls="navbar-items" aria-expanded="false" aria-label="Toggle navigation">
             <i className="bi bi-list text-white"></i>
           </button>
           <div className="collapse navbar-collapse" id="navbar-items">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
             <li className="nav-item">
-              <a href="#quemsoueu" id="btnquemsoueu" className="nav-link active text-white montserrat"
-                aria-current="page">Quem sou eu</a>
-              <a href="#formacao" id="btnformacao" className="nav-link active text-white montserrat">Formação</a>
-              <a href="#projetos" id="btnprojetos" className="nav-link active text-white montserrat">Projetos</a>
-              <a href="#contato" id="btncontato" className="nav-link active text-white montserrat">Contato</a>
+              <a href="#quemsoueu" className="nav-link active text-white montserrat" aria-current="page">Quem sou eu</a>
+              <a href="#formacao" className="nav-link active text-white montserrat">Formação</a>
+              <a href="#projetos" className="nav-link active text-white montserrat">Projetos</a>
+              <a href="#contato" className="nav-link active text-white montserrat">Contato</a>
             </li>
           </div>
         </div>
